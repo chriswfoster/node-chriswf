2087c705d5abe178469437228d2778065460151e
Chris Foster <chriswfoster@gmail.com>
2017-11-30 15:21:06 -0600

    make it better

---
+++ BACKUP/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    domain: 'chriswf.auth0.com',
+    clientID: 'hs7pQHdK4zS3HJ3r6PS1lu8pdL_SWW8n',
+    clientSecret: 'U8UT2e8O96LwDtsBhtu13CU2hbNuKY_78FxrMdHtTGFbQFooAGuhAoVxziesZc9k',
+    secret: 'supersolidsecret'
+}
---
+++ config.js
@@ -0,0 +1,15 @@
+
+//set up the stuff to be exported
+module.exports = {
+massive: {
+    connectionString: 'postgres://phnfqiilsbbhsl:e62da1300d6a0694ac45293a877f8513740c8be7ba5b092a73ba878a019abf85@ec2-54-243-58-69.compute-1.amazonaws.com:5432/d6a46s6q8jkb4p?ssl=true'
+    },
+    session: {
+        secret: 'supersecret'
+    },
+    auth0: {
+        domain: 'chriswf.auth0.com',
+        clientID: 'hs7pQHdK4zS3HJ3r6PS1lu8pdL_SWW8n',
+        clientSecret:'U8UT2e8O96LwDtsBhtu13CU2hbNuKY_78FxrMdHtTGFbQFooAGuhAoVxziesZc9k'
+    }
+}
